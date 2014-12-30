a8f719db7e595494e064efc392f3cde554cfa893
Matthew Honnibal <honnibal@gmail.com>
2014-12-30 17:28:25 +1100

    * Add setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from Cython.Build import cythonize
+from Cython.Distutils import Extension
+import distutils.core
+import os
+from os import path
+from glob import glob
+
+
+includes = ['murmurhash/']
+if 'VIRTUAL_ENV' in os.environ:
+    includes += glob(path.join(os.environ['VIRTUAL_ENV'], 'include', 'site', '*'))
+else:
+    # If you're not using virtualenv, set your include dir here.
+    pass
+
+exts = [
+    Extension('murmurhash.mrmr', ["murmurhash/mrmr.pyx", "murmurhash/MurmurHash2.cpp",
+              "murmurhash/MurmurHash3.cpp"], language="c++", include_dirs=includes),
+]
+
+
+distutils.core.setup(
+    name='murmurhash',
+    packages=['murmurhash'],
+    package_data={'murmurhash': ['*.h', '*.pxd']},
+    author='Matthew Honnibal',
+    author_email='honnibal@gmail.com',
+    version='0.17',
+    ext_modules=cythonize(exts),
+    classifiers=[
+                'Development Status :: 4 - Beta',
+                'Environment :: Console',
+                'Operating System :: OS Independent',
+                'Intended Audience :: Science/Research',
+                'Programming Language :: Cython',
+                'Topic :: Scientific/Engineering'],
+    headers=["murmurhash/MurmurHash2.h", "murmurhash/MurmurHash3.h"]
+)
