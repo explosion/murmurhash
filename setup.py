cd1a5cf9049ef6c43f63911c8bb875872716c903
Matthew Honnibal <honnibal@gmail.com>
2015-01-03 13:03:37 +1100

    * Fix MurmurHash setup for Cython

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import distutils.core
+import setuptools
 import os
 from os import path
 from glob import glob
@@ -8,6 +8,7 @@ from glob import glob
 includes = ['murmurhash/']
 if 'VIRTUAL_ENV' in os.environ:
     includes += glob(path.join(os.environ['VIRTUAL_ENV'], 'include', 'site', '*'))
+    includes += glob(path.join(os.environ['VIRTUAL_ENV'], 'include', '*'))
 else:
     # If you're not using virtualenv, set your include dir here.
     pass
@@ -18,7 +19,7 @@ try:
     from Cython.Build import cythonize
     from Cython.Distutils import Extension
 
-    cythonize(exts = [
+    exts=cythonize([
         Extension('murmurhash.mrmr', ["murmurhash/mrmr.pyx", "murmurhash/MurmurHash2.cpp",
               "murmurhash/MurmurHash3.cpp"], language="c++", include_dirs=includes),
     ])
@@ -30,13 +31,13 @@ except ImportError:
     ]
 
 
-distutils.core.setup(
+setuptools.setup(
     name='murmurhash',
     packages=['murmurhash'],
     package_data={'murmurhash': ['*.pyx', '*.h', '*.pxd']},
     author='Matthew Honnibal',
     author_email='honnibal@gmail.com',
-    version='0.18',
+    version='0.20',
     ext_modules=exts,
     classifiers=[
                 'Development Status :: 4 - Beta',
