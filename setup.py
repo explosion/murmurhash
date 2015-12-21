d4a2a6da50559c635cd3ab30074b74228521af4b
Henning Peters <pete@dexterslab.de>
2015-12-21 11:18:27 +0100

    new setup.py (replaces install_headers workaround)

--- setup.py
+++ setup.py
@@ -1,65 +1,211 @@
 #!/usr/bin/env python
-import setuptools
-import distutils
-import distutils.core
-import distutils.sysconfig
+from __future__ import print_function
 import os
-from os import path
+import subprocess
 import sys
-from glob import glob
-import shutil
-
-
-distutils.sysconfig.get_config_vars()
-
-
-def install_headers():
-    dest_dir = path.join(sys.prefix, 'include', 'murmurhash')
-    if not path.exists(dest_dir):
-        shutil.copytree('murmurhash/headers/murmurhash', dest_dir)
-
-def rm_cflag(text):
-    cflags = distutils.sysconfig._config_vars['CFLAGS']
-    cflags = cflags.replace(text, '')
-    distutils.sysconfig._config_vars['CFLAGS'] = cflags
-
-install_headers()
-includes = ['.', path.join(sys.prefix, 'include')]
-
-rm_cflag('-fno-strict-aliasing')
-rm_cflag('-Wstrict-prototypes')
-rm_cflag('-NDEBUG')
-
+import contextlib
+from distutils.command.build_ext import build_ext
+from distutils.sysconfig import get_python_inc
 
 try:
-    from Cython.Build import cythonize
-    from Cython.Distutils import Extension
-
-    exts=cythonize([
-        Extension('murmurhash.mrmr', ["murmurhash/mrmr.pyx", "murmurhash/MurmurHash2.cpp",
-              "murmurhash/MurmurHash3.cpp"], language="c++", include_dirs=includes),
-    ])
+    from setuptools import Extension, setup
 except ImportError:
-    from distutils.extension import Extension
-    exts = [
-        Extension('murmurhash.mrmr', ["murmurhash/mrmr.cpp", "murmurhash/MurmurHash2.cpp",
-              "murmurhash/MurmurHash3.cpp"], language="c++", include_dirs=includes),
-    ]
+    from distutils.core import Extension, setup
 
 
-setuptools.setup(
-    name='murmurhash',
-    packages=['murmurhash'],
-    package_data={'murmurhash': ['*.pyx', 'headers/murmurhash/*.h', '*.pxd']},
-    author='Matthew Honnibal',
-    author_email='honnibal@gmail.com',
-    version='0.24',
-    ext_modules=exts,
-    classifiers=[
-                'Development Status :: 4 - Beta',
-                'Environment :: Console',
-                'Operating System :: OS Independent',
-                'Intended Audience :: Science/Research',
-                'Programming Language :: Cython',
-                'Topic :: Scientific/Engineering'],
-)
+MAJOR      = 0
+MINOR      = 24
+MICRO      = 0
+ISRELEASED = False
+VERSION    = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+
+PACKAGES = ['murmurhash']
+
+
+MOD_NAMES = ['murmurhash.mrmr']
+
+
+if sys.version_info[:2] < (2, 7) or (3, 0) <= sys.version_info[0:2] < (3, 4):
+    raise RuntimeError('Python version 2.7 or >= 3.4 required.')
+
+
+# By subclassing build_extensions we have the actual compiler that will be used which is really known only after finalize_options
+# http://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
+compile_options =  {'msvc'  : ['/Ox', '/EHsc'],
+                    'other' : ['-O3', '-Wno-strict-prototypes', '-Wno-unused-function']}
+link_options    =  {'msvc'  : [],
+                    'other' : []}
+
+if sys.platform.startswith('darwin'):
+    compile_options['other'].append('-mmacosx-version-min=10.8')
+    compile_options['other'].append('-stdlib=libc++')
+    link_options['other'].append('-lc++')
+
+
+class build_ext_options:
+    def build_options(self):
+        for e in self.extensions:
+            e.extra_compile_args = compile_options.get(
+                self.compiler.compiler_type, compile_options['other'])
+        for e in self.extensions:
+            e.extra_link_args = link_options.get(
+                self.compiler.compiler_type, link_options['other'])
+
+
+class build_ext_subclass(build_ext, build_ext_options):
+    def build_extensions(self):
+        build_ext_options.build_options(self)
+        build_ext.build_extensions(self)
+
+
+# Return the git revision as a string
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = 'Unknown'
+
+    return GIT_REVISION
+
+
+def get_version_info():
+    # Adding the git rev number needs to be done inside write_version_py(),
+    # otherwise the import of murmurhash.about messes up the build under Python 3.
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    elif os.path.exists(os.path.join('murmurhash', 'about.py')):
+        # must be a source distribution, use existing version file
+        try:
+            from murmurhash.about import git_revision as GIT_REVISION
+        except ImportError:
+            raise ImportError('Unable to import git_revision. Try removing '
+                              'murmurhash/about.py and the build directory '
+                              'before building.')
+    else:
+        GIT_REVISION = 'Unknown'
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev0+' + GIT_REVISION[:7]
+
+    return FULLVERSION, GIT_REVISION
+
+
+def write_version(path):
+    cnt = """# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+if not release:
+    version = full_version
+"""
+    FULLVERSION, GIT_REVISION = get_version_info()
+
+    with open(path, 'w') as f:
+        f.write(cnt % {'version': VERSION,
+                       'full_version' : FULLVERSION,
+                       'git_revision' : GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+
+
+def generate_cython(root, source):
+    print('Cythonizing sources')
+    p = subprocess.call([sys.executable,
+                         os.path.join(root, 'bin', 'cythonize.py'),
+                         source])
+    if p != 0:
+        raise RuntimeError('Running cythonize failed')
+
+
+def is_source_release(path):
+    return os.path.exists(os.path.join(path, 'PKG-INFO'))
+
+
+def clean(path):
+    for name in MOD_NAMES:
+        name = name.replace('.', '/')
+        for ext in ['.so', '.html', '.cpp', '.c']:
+            file_path = os.path.join(path, name + ext)
+            if os.path.exists(file_path):
+                os.unlink(file_path)
+
+
+@contextlib.contextmanager
+def chdir(new_dir):
+    old_dir = os.getcwd()
+    try:
+        os.chdir(new_dir)
+        sys.path.insert(0, new_dir)
+        yield
+    finally:
+        del sys.path[0]
+        os.chdir(old_dir)
+
+
+def setup_package():
+    root = os.path.abspath(os.path.dirname(__file__))
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'clean':
+        return clean(root)
+
+    with chdir(root):
+        write_version(os.path.join(root, 'murmurhash', 'about.py'))
+
+        include_dirs = [
+            get_python_inc(plat_specific=True),
+            os.path.join(root, 'murmurhash', 'headers')]
+
+        ext_modules = []
+        for mod_name in MOD_NAMES:
+            mod_path = mod_name.replace('.', '/') + '.cpp'
+            ext_modules.append(
+                Extension(mod_name, [mod_path,
+                                     'murmurhash/MurmurHash2.cpp',
+                                     'murmurhash/MurmurHash3.cpp'],
+                    language='c++', include_dirs=include_dirs))
+
+        if not is_source_release(root):
+            generate_cython(root, 'murmurhash')
+
+        setup(
+            name='murmurhash',
+            packages=PACKAGES,
+            package_data={'murmurhash': ['headers/murmurhash/*.h']},
+            description='Cython bindings for MurmurHash2',
+            author='Matthew Honnibal',
+            author_email='matt@spacy.io',
+            version=VERSION,
+            url='https://github.com/syllog1sm/cython-murmurhash',
+            license='MIT',
+            ext_modules=ext_modules,
+            classifiers=['Development Status :: 4 - Beta',
+                         'Environment :: Console',
+                         'Operating System :: OS Independent',
+                         'Intended Audience :: Science/Research',
+                         'Programming Language :: Cython',
+                         'Topic :: Scientific/Engineering'],
+            cmdclass = {
+                'build_ext': build_ext_subclass},
+        )
+
+
+if __name__ == '__main__':
+    setup_package()
