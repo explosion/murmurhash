f3c9676f78a3359d30b1401b87d1335731693654
Matthew Honnibal <honnibal+gh@gmail.com>
2016-02-19 15:20:48 +0100

    * Increment version

--- murmurhash/about.py
+++ murmurhash/about.py
@@ -4,10 +4,10 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
 __name__ = 'murmurhash'
-__version__ = '0.25.0'
+__version__ = '0.27.0'
 __summary__ = 'Cython bindings for MurmurHash2'
 __uri__ = 'https://github.com/syllog1sm/cython-murmurhash'
 __author__ = 'Matthew Honnibal'
 __email__ = 'matt@spacy.io'
 __license__ = 'MIT'
-__release__ = False
+__release__ = True
