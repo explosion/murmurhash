4681f35259643a6278e2ee02d3b5e676d9d32b81
Henning Peters <pete@dexterslab.de>
2016-03-13 23:02:05 +0100

    cleanup

--- murmurhash/about.py
+++ murmurhash/about.py
@@ -3,11 +3,10 @@
 # https://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
-__name__ = 'murmurhash'
+__title__ = 'murmurhash'
 __version__ = '0.25.0'
 __summary__ = 'Cython bindings for MurmurHash2'
-__uri__ = 'https://github.com/syllog1sm/cython-murmurhash'
+__uri__ = 'https://github.com/spacy-io/cython-murmurhash'
 __author__ = 'Matthew Honnibal'
 __email__ = 'matt@spacy.io'
 __license__ = 'MIT'
-__release__ = False
--- setup.py
+++ setup.py
@@ -88,13 +88,16 @@ def setup_package():
         return clean(root)
 
     with chdir(root):
-        about = {}
         with open(os.path.join(root, 'murmurhash', 'about.py')) as f:
+            about = {}
             exec(f.read(), about)
+
+        with open(os.path.join(root, 'README.rst')) as f:
+            readme = f.read()
 
         include_dirs = [
             get_python_inc(plat_specific=True),
-            os.path.join(root, 'murmurhash', 'headers')]
+            os.path.join(root, 'include')]
 
         ext_modules = []
         for mod_name in MOD_NAMES:
@@ -109,24 +112,34 @@ def setup_package():
             generate_cython(root, 'murmurhash')
 
         setup(
-            name=about['__name__'],
+            name=about['__title__'],
+            zip_safe=True,
             packages=PACKAGES,
-            package_data={'murmurhash': ['*.pyx',
-                                         '*.pxd',
-                                         'headers/murmurhash/*.h']},
+            package_data={'': ['*.pyx', '*.pxd']},
             description=about['__summary__'],
+            long_description=readme,
             author=about['__author__'],
             author_email=about['__email__'],
             version=about['__version__'],
             url=about['__uri__'],
             license=about['__license__'],
             ext_modules=ext_modules,
-            classifiers=['Development Status :: 4 - Beta',
-                         'Environment :: Console',
-                         'Operating System :: OS Independent',
-                         'Intended Audience :: Science/Research',
-                         'Programming Language :: Cython',
-                         'Topic :: Scientific/Engineering'],
+            classifiers=[
+                'Development Status :: 4 - Beta',
+                'Environment :: Console',
+                'Intended Audience :: Developers',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: MIT License',
+                'Operating System :: POSIX :: Linux',
+                'Operating System :: MacOS :: MacOS X',
+                'Operating System :: Microsoft :: Windows',
+                'Programming Language :: Cython',
+                'Programming Language :: Python :: 2.6',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3.3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Topic :: Scientific/Engineering'],
             cmdclass = {
                 'build_ext': build_ext_subclass},
         )
