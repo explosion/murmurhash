77ebe09a9d04513b5dc33d616bf95ce6f6c1ef52
Matthew Honnibal <honnibal+gh@gmail.com>
2017-06-01 02:24:47 -0500

    Add 128-bit function to API. Move extern to .pyx, to avoid header problem

--- murmurhash/mrmr.pxd
+++ murmurhash/mrmr.pxd
@@ -1,14 +1,8 @@
 from libc.stdint cimport uint64_t, int64_t, uint32_t
 
-cdef extern from "murmurhash/MurmurHash3.h":
-    void MurmurHash3_x86_32(void * key, uint64_t len, uint64_t seed, void* out) nogil
-    void MurmurHash3_x86_128(void * key, uint64_t len, uint64_t seed, void* out) nogil
-
-cdef extern from "murmurhash/MurmurHash2.h":
-    uint64_t MurmurHash64A(void * key, int length, uint32_t seed) nogil
-    uint64_t MurmurHash64B(void * key, int length, uint32_t seed) nogil
-
 
 cdef uint32_t hash32(void* key, int length, uint32_t seed) nogil
 cdef uint64_t hash64(void* key, int length, uint64_t seed) nogil
 cdef uint64_t real_hash64(void* key, int length, uint64_t seed) nogil
+cdef void hash128_x86(const void* key, int len, uint32_t seed, void* out) nogil
+cdef void hash128_x64(const void* key, int len, uint32_t seed, void* out) nogil
--- murmurhash/mrmr.pyx
+++ murmurhash/mrmr.pyx
@@ -1,6 +1,16 @@
 from libc.stdint cimport uint64_t, int64_t, int32_t
 
 
+cdef extern from "murmurhash/MurmurHash3.h":
+    void MurmurHash3_x86_32(void * key, uint64_t len, uint64_t seed, void* out) nogil
+    void MurmurHash3_x86_128(void * key, int len, uint32_t seed, void* out) nogil
+    void MurmurHash3_x64_128(void * key, int len, uint32_t seed, void* out) nogil
+
+cdef extern from "murmurhash/MurmurHash2.h":
+    uint64_t MurmurHash64A(void * key, int length, uint32_t seed) nogil
+    uint64_t MurmurHash64B(void * key, int length, uint32_t seed) nogil
+
+
 cdef uint32_t hash32(void* key, int length, uint32_t seed) nogil:
     cdef int32_t out
     MurmurHash3_x86_32(key, length, seed, &out)
@@ -14,3 +24,11 @@ cdef uint64_t real_hash64(void* key, int length, uint64_t seed) nogil:
     cdef uint64_t[2] out
     MurmurHash3_x86_128(key, length, seed, &out)
     return out[1]
+
+
+cdef void hash128_x86(const void* key, int length, uint32_t seed, void* out) nogil:
+    MurmurHash3_x86_128(key, length, seed, out)
+
+
+cdef void hash128_x64(const void* key, int length, uint32_t seed, void* out) nogil:
+    MurmurHash3_x64_128(key, length, seed, out)
