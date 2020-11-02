4514aba675e01cba7d1948e66c729d2be5108b94
Adriane Boyd <adrianeboyd@gmail.com>
2020-11-02 10:40:51 +0100

    Remove wheel version pin (#17)
    * Remove wheel version pin
    
    * Set version to v1.0.4

--- murmurhash/about.py
+++ murmurhash/about.py
@@ -1,5 +1,5 @@
 __title__ = "murmurhash"
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __summary__ = "Cython bindings for MurmurHash"
 __uri__ = "https://github.com/explosion/murmurhash"
 __author__ = "Matthew Honnibal"
